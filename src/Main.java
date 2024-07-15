643abba3a127abf358bb2e96d8f36ca0f15a126a
quhead <quququhead@gmail.com>
2024-07-15 13:23:38 +0300

    Добавить реализацию для истории просмотров

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import model.Epic;
+import model.Status;
+import model.Subtask;
 import service.InMemoryTaskManager;
 
 public class Main {
@@ -5,5 +8,40 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Поехали!");
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
+
+        Epic epic = new Epic("Отпраздновать ДР", "На выходных");
+        Subtask subtask1 = new Subtask("Собрать друзей", "Написать всем", Status.NEW, 0);
+        Subtask subtask2 = new Subtask("Закупиться", "Больше еды и напитков", Status.NEW, 0);
+        Subtask subtask3 = new Subtask("Найти домик", "На природе с баней", Status.NEW, 0);
+        Epic emptyEpic = new Epic("Пустое название", "Пустое описание");
+
+        inMemoryTaskManager.addTask(epic);
+        inMemoryTaskManager.addTask(subtask1);
+        inMemoryTaskManager.addTask(subtask2);
+        inMemoryTaskManager.addTask(subtask3);
+        inMemoryTaskManager.addTask(emptyEpic);
+
+        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println(inMemoryTaskManager.getEpic(0));
+        System.out.println(inMemoryTaskManager.getSubtask(1));
+        System.out.println(inMemoryTaskManager.getSubtask(2));
+        System.out.println(inMemoryTaskManager.getSubtask(3));
+        System.out.println(inMemoryTaskManager.getEpic(4));
+        System.out.println("-".repeat(60));
+        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println("-".repeat(60));
+        System.out.println(inMemoryTaskManager.getSubtask(2));
+        System.out.println(inMemoryTaskManager.getSubtask(3));
+        System.out.println(inMemoryTaskManager.getSubtask(1));
+        System.out.println(inMemoryTaskManager.getEpic(4));
+        System.out.println(inMemoryTaskManager.getEpic(0));
+        System.out.println("-".repeat(60));
+        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println("-".repeat(60));
+        inMemoryTaskManager.removeEpic(4);
+        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println("-".repeat(60));
+        inMemoryTaskManager.removeEpic(0);
+        System.out.println(inMemoryTaskManager.getHistory());
     }
 }
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -3,25 +3,108 @@ package service;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history;
+    private final Map<Integer, Node> history;
+    public Node head;
+    public Node tail;
 
     public InMemoryHistoryManager() {
-        history = new ArrayList<>();
+        history = new HashMap<>();
+        head = null;
+        tail = null;
     }
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        List<Task> historyList = new ArrayList<>();
+        if (!(history.isEmpty())) {
+            Node node = head;
+            for (int i = 0; i < history.size(); i++) {
+                historyList.add(node.getData());
+                node = node.getNext();
+            }
+        }
+        return historyList;
     }
 
     @Override
     public void add(Task task) {
-        if (history.size() == 10) {
-            history.removeFirst();
+        remove(task.getId());
+        Node node = new Node(task);
+        linkLast(node);
+        history.put(task.getId(), node);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node node = history.get(id);
+        if (node != null) {
+            removeNode(node);
+            history.remove(id);
         }
-        history.add(task);
+    }
+
+    private void linkLast(Node node) {
+        if (history.isEmpty()) {
+            head = node;
+        } else {
+            tail.setNext(node);
+            node.setPrev(tail);
+        }
+        tail = node;
+    }
+
+    private void removeNode(Node node) {
+        Node prev = node.getPrev();
+        Node next = node.getNext();
+        if (prev == null && next == null) {
+            head = null;
+            tail = null;
+        } else if (prev == null) {
+            next.setPrev(null);
+            head = next;
+        } else if (next == null) {
+            prev.setNext(null);
+            tail = prev;
+        } else {
+            prev.setNext(next);
+            next.setPrev(prev);
+        }
+    }
+}
+
+final class Node {
+    Node next;
+    Task data;
+    Node prev;
+
+    Node(Task task) {
+        data = task;
+        next = null;
+        prev = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Task getData() {
+        return data;
     }
 }

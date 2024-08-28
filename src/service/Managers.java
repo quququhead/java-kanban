73bf02a683373555b643b4ae6c7d5f5df2a0b9e5
quhead <quququhead@gmail.com>
2024-08-28 08:17:36 +0300

    Fix autoTest

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -10,7 +10,8 @@ import java.time.LocalDateTime;
 
 public class Managers {
 
-    private Managers() {}
+    private Managers() {
+    }
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
